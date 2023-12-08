ff84799acbe34adf8202b0fc9dbe29623f230b01
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-07 21:48:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ tab1, tab2, tab3 = st.tabs(["Option 1", "Option 2", "Option 3"])
 with tab1:
    st.header("A cat")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-   st.components.v1.iframe("""<iframe src="https://30days.streamlit.app/?embed=true" height="450" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
+   st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height="450" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
 
 with tab2:
    st.header("A dog")
