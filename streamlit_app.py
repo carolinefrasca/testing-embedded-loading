767b6231b8d637ed9bfb60ffc2bedbf8bbfca222
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-08 16:11:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,21 @@ tab1, tab2, tab3 = st.tabs(["Option 1", "Option 2", "Option 3"])
 with tab1:
    st.header("A cat")
    # st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-   col1, col2 = st.columns(2)
-
-   code = """
-   import streamlit as st
-
-   st.text('This is some text.')
-   """
-   with col1:
+   show_code = st.checkbox("Show code")
+   if show_code:
+      col1, col2 = st.columns(2)
+   
+      code = """
+      import streamlit as st
+   
+      st.text('This is some text.')
+      """
+      with col1:
+         st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height=1200 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
+      with col2:
+         st.code(code)
+   else:
       st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height=1200 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
-   with col2:
-      st.code(code)
 with tab2:
    col1, col2 = st.columns(2)
 
