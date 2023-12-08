ef9f8f412148bc7ea5578faaab44b1bb2cb8a7dd
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-07 21:45:40 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+tab1, tab2, tab3 = st.tabs(["Option 1", "Option 2", "Option 3"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   st.components.v1.iframe("""<iframe src="https://30days.streamlit.app/?embed=true" height="450" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
