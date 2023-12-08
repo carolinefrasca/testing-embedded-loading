aa86b1f019cc05e0f8536c5ece95736a4334d152
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-08 15:48:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,15 @@ tab1, tab2, tab3 = st.tabs(["Option 1", "Option 2", "Option 3"])
 with tab1:
    st.header("A cat")
    # st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-   st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
+   st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height=1200 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
 
 with tab2:
    st.header("A dog")
    # st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-   st.components.v1.html("""<iframe src="https://cross-chain-monitoring.streamlit.app/?embed=true" style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
+   st.components.v1.html("""<iframe src="https://cross-chain-monitoring.streamlit.app/?embed=true" height=1200 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
 
 
 with tab3:
    st.header("An owl")
-   st.components.v1.html("""<iframe src="https://gptlab.streamlit.app/?embed=true" style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
+   st.components.v1.html("""<iframe src="https://gptlab.streamlit.app/?embed=true" height=1200 style="width:100%;border:none;"></iframe>""", width=None, height=1200, scrolling=False)
    # st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
