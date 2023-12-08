d8af31af54c894dd0fc1f22ab8852cae7e78b731
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-08 15:44:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,15 @@ tab1, tab2, tab3 = st.tabs(["Option 1", "Option 2", "Option 3"])
 
 with tab1:
    st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-   st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height="450" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
+   # st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   st.components.v1.html("""<iframe src="https://30days.streamlit.app/?embed=true" height="800" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
 
 with tab2:
    st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   # st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+   st.components.v1.html("""<iframe src="https://doc-column-config-overview.streamlit.app/?embed=true" height="800" style="width:100%;border:none;"></iframe>""", width=None, height=None, scrolling=False)
+
 
 with tab3:
    st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   # st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
